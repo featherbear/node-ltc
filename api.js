2b79e592486697cf9694d5e220d48c2fa7b76bee
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-26 00:42:05 +1000

    Export types from the library

--- api.js
+++ api.js
@@ -2,3 +2,4 @@ const ffi = require('ffi-napi')
 
 // Library: /usr/local/lib/libltc.so
 module.exports = ffi.Library('./build/Release/libltc', require('./lib/functions/'))
+module.exports.types = require('./lib/types')
