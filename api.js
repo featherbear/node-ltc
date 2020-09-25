36d2289f7b4f1b8c5ff84e993cfa958c42f04b28
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-26 01:13:37 +1000

    Add sound buffer creator

--- api.js
+++ api.js
@@ -2,4 +2,6 @@ const ffi = require('ffi-napi')
 
 // Library: /usr/local/lib/libltc.so
 module.exports = ffi.Library('./build/Release/libltc', require('./lib/functions/'))
+
 module.exports.types = require('./lib/types')
+module.exports.util = require('./lib/util')
