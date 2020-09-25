9856dcbef936d310128bcfcccac2da42889a8240
Andrew Wong <andrew.j.wong@outlook.com>
2020-09-25 21:58:22 +1000

    Add wrapper for device open

--- naudiodon-test.js
+++ naudiodon-test.js
@@ -1,23 +1,14 @@
-const portAudio = require('naudiodon')
+const portAudio = require('./portaudioLib')
 
-let inputDevices = portAudio.getDevices().filter(({maxInputChannels: inputs}) => inputs > 0)
-console.log(inputDevices.find(({name})=>name=="HDA Intel PCH: ALC3253 Analog (hw:0,0)"));
+let inputDevices = portAudio.getInputDevices()
 
-// Create an instance of AudioIO with inOptions (defaults are as below), which will return a ReadableStream
+let device = inputDevices.default.find(({ id }) => id == 1)
+let stream = portAudio.createStream(device)
+
 const fs = require('fs')
-var ai = new portAudio.AudioIO({
-    inOptions: {
-      channelCount: 2,
-      sampleFormat: portAudio.SampleFormat16Bit,
-      sampleRate: 44100,
-      deviceId: -1, // Use -1 or omit the deviceId to select the default device
-      closeOnError: true // Close the stream if an audio error is detected, if set false then just log the error
-    }
-  });
-  
-  // Create a write stream to write out to a raw audio file
-  var ws = fs.createWriteStream('rawAudio.raw');
-  
-  //Start streaming
-  ai.pipe(ws);
-  ai.start();
+var ws = fs.createWriteStream('rawAudio.raw')
+
+stream.pipe(ws)
+stream.start()
+
+// https://www.npmjs.com/package/microphone-stream
